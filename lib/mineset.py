8fca3a2472ab0b82dcfce9be403846d35ac8c0a4
uc-cjkhoh <cj_khoh@unifiedcomms.com>
2025-05-14 08:31:44 +0800

    initial commit

--- lib/mineset.py
+++
@@ -1,9 +0,0 @@
-class MineSet:
-    def __init__(self):
-        self.running_mode = 1
-        self.module_name = ''
-        self.hive_reload = 1
-        self.impala_reload = 1
-        
-        self.total_mine = 1
-        
