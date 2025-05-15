b471f4685330e6b64dd5e5af1fed40b700c102b6
uc-cjkhoh <cj_khoh@unifiedcomms.com>
2025-05-15 10:18:55 +0800

    point anomalies: optimization

--- lib/model.py
+++ lib/model.py
@@ -62,19 +62,17 @@ class MachineLearning:
         return svm.fit_predict(data) == 1
  
 
-def detect_point_anomalies(data: pd.Series, threshold: int, window_size: int) -> np.ndarray:
+def detect_point_anomalies(data: pd.Series, threshold: int, window_size: int) -> np.ndarray: 
     mean_12_loop = data.rolling(window=window_size * 12).mean()
     mean_24_loop = data.rolling(window=window_size * 24).mean()
-    std_12_loop = data.rolling(window=window_size * 12).std()
-    std_24_loop = data.rolling(window=window_size * 24).std()
-    
+      
     means = [mean_12_loop, mean_24_loop]
-    stds = [std_12_loop, std_24_loop]
+    std = data.std()
     
     final_result = np.zeros(data.shape[0])
     
     # calculate lower and upper boundaries
-    for mean, std in zip(means, stds):
+    for mean in means:
         lower_boundary = mean - (threshold * std)
         upper_boundary = mean + (threshold * std)
 
